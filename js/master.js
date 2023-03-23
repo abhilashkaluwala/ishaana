6b32ab7035765dba0006fba3bb7bfb087c8e69cb
abhilashkaluwala <lashabhi37@gmail.com>
2023-03-23 13:58:34 +0530

    media query for mobile navbar

--- js/master.js
+++ js/master.js
@@ -517,5 +517,17 @@ function changeImage2() {
             
             
 		});
+
+        function myFunction() {
+            var x = document.getElementById("navbar-nav");
+            if (x.style.display === "block") {
+              x.style.display = "none";
+            } else {
+              x.style.display = "block";
+            }
+          }
+
+
+    
  
   
